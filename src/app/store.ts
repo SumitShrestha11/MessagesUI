7c25442ef2f4e3b41613bbac24e34e67543738fb
Sumit <sumit.sth54@gmail.com>
2023-08-01 21:24:19 +0545

    fix: misleading comment

--- src/app/store.ts
+++ src/app/store.ts
@@ -11,5 +11,5 @@ export default store;
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {messages: IStat}
+// Inferred type: {messages: IState}
 export type AppDispatch = typeof store.dispatch;
